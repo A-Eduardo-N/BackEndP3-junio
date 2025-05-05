7088b2c4506a14396ab390a4d29d5387b7631aa2
A-Eduardo-N <164400460+A-Eduardo-N@users.noreply.github.com>
2025-05-05 15:36:03 -0300

    Delete projetoBD.1.2 directory

--- projetoBD.1.2/src/main/resources/db/migration/V03__Create_Payment_Table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tb_payment (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    moment TIMESTAMP NOT NULL,
-    order_id BIGINT UNIQUE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES tb_order(id)
-);
--- projetoBD.1.2/src/main/resources/db/migration/V06__Create_Product_Table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tb_product (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    price DOUBLE NOT NULL,
-    imgUrl VARCHAR(255)
-);
